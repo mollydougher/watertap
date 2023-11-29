31cd0926553d06cf979bbacbed36799337657bb6
Molly Dougher <mdoughe6@nd.edu>
2023-11-29 15:41:56 -0500

    reformulated flowsheet into functions and matched from A.Dudchenko file

--- watertap/examples/flowsheets/nf_dspmde/tests/nf_li_mg_fromtest.py
+++ watertap/examples/flowsheets/nf_dspmde/tests/nf_li_mg_fromtest.py
@@ -36,7 +36,6 @@ from watertap.property_models.multicomp_aq_sol_prop_pack import (
     DensityCalculation,
     MCASStateBlock,
 )
-from watertap.unit_models.pressure_changer import Pump
 from watertap.unit_models.nanofiltration_DSPMDE_0D import (
     NanofiltrationDSPMDE0D,
     MassTransferCoefficient,
@@ -59,154 +58,145 @@ from idaes.core.util.scaling import (
 )
 from idaes.core.util.initialization import propagate_state
 from idaes.models.unit_models import (
-    Feed, Product
+    Feed
 )
 
-# define the default solver
-solver = get_solver
+# main function from Alex Dudchenko
+def main():
+    for k in np.linspace(1, 10, 100):
+        try:
+            m = build()
+            # inlet_pressure = (
+            #     m.fs.unit.feed_side.properties_in[0.0].pressure_osm_phase["Liq"].value
+            # )
+            m.fs.unit.inlet.pressure[0].fix(k * 1e5)
+            m.fs.unit.initialize()
+            print("init_okay", k)
+            m.fs.unit.report()
+            break
+        except:
+            pass
 
-# create the model
-m = ConcreteModel()
 
-# create the flowsheet
-m.fs = FlowsheetBlock(dynamic = False)
+def build():
+    # create the model
+    m = ConcreteModel()
 
-# define the propery model
-# define the properties
-property_kwds = {
-# need to add Cl- for electroneutrality, assume LiCl and MgCl2 salts
-"solute_list": [
-    "Li_+",
-    "Mg_2+",
-    "Cl_-",
-],
-# https://www.aqion.de/site/diffusion-coefficients
-# very confident
-"diffusivity_data": {
-    ("Liq","Li_+"): 1.03e-09,
-    ("Liq","Mg_2+"): 0.075e-09,
-    ("Liq","Cl_-"): 2.03e-09
-}, 
-# very confident
-"mw_data": {
-    "H2O": 0.018,
-    "Li_+": 0.0069,
-    "Mg_2+": 0.024,
-    "Cl_-": 0.035
-},
-# avg vals from https://www.sciencedirect.com/science/article/pii/S138358661100637X
-# medium confident, these values come from above review paper, averaged values from multiple studies
-# reasonable orders of magnitude
-"stokes_radius_data": {
-    #"Cl_-": 0.121e-9,
-    "Li_+": 3.61e-10,
-    "Mg_2+": 4.07e-10,
-    "Cl_-": 3.28e-10
-},
-# very confident
-"charge": {
-    "Li_+": 1,
-    "Mg_2+": 2,
-    "Cl_-": -1
-},
-# choose ideal for now, other option is davies
-"activity_coefficient_model": ActivityCoefficientModel.ideal,
-}
+    # create the flowsheet
+    m.fs = FlowsheetBlock(dynamic = False)
 
-m.fs.properties = MCASParameterBlock(**property_kwds)
-#density_calculation=DensityCalculation.constant
+    # define the propery model
+    m.fs.properties = MCASParameterBlock(
+         # need to add Cl- for electroneutrality, assume LiCl and MgCl2 salts
+        solute_list = ["Li_+","Mg_2+","Cl_-"],
 
-# add feed, product, and disposal blocks
-m.fs.feed = Feed(property_package=m.fs.properties)
-m.fs.product = Product(property_package=m.fs.properties)
-m.fs.disposal = Product(property_package=m.fs.properties)
+        # https://www.aqion.de/site/diffusion-coefficients
+        # very confident
+         diffusivity_data=  {
+            ("Liq","Li_+"): 1.03e-09,
+            ("Liq","Mg_2+"): 0.705e-09,
+            ("Liq","Cl_-"): 2.03e-09
+        }, 
 
-# define unit models
-m.fs.pump = Pump(property_package=m.fs.properties)
-m.fs.unit = NanofiltrationDSPMDE0D(property_package = m.fs.properties)
+        # very confident
+        mw_data = {
+            "H2O": 0.018,
+            "Li_+": 0.0069,
+            "Mg_2+": 0.024,
+            "Cl_-": 0.035
+        },
 
-# connect the units
-m.fs.feed_to_pump = Arc(source=m.fs.feed.outlet, destination=m.fs.pump.inlet)
-m.fs.pump_to_nf = Arc(source=m.fs.pump.outlet, destination=m.fs.unit.inlet)
-m.fs.nf_to_product = Arc(source=m.fs.unit.permeate, destination=m.fs.product.inlet)
-m.fs.nf_to_disposal = Arc(source=m.fs.unit.retentate, destination=m.fs.disposal.inlet)
-TransformationFactory("network.expand_arcs").apply_to(m)
+        # avg vals from https://www.sciencedirect.com/science/article/pii/S138358661100637X
+        # medium confident, these values come from above review paper, averaged values from multiple studies
+        # reasonable orders of magnitude
+        stokes_radius_data = {
+            "Cl_-": 0.121e-9,
+            "Li_+": 3.61e-10,
+            "Mg_2+": 4.07e-10,
+            #"Cl_-": 3.28e-10
+        },
 
-# fix the inlet flow rates
-# kept all values from the WaterTAP test.py for now
-m.fs.unit.inlet.flow_mol_phase_comp[0, "Liq", "Li_+"].fix(0.429868)
-m.fs.unit.inlet.flow_mol_phase_comp[0, "Liq", "Mg_2+"].fix(0.429868)
-m.fs.unit.inlet.flow_mol_phase_comp[0, "Liq", "Cl_-"].fix(0.429868)
-m.fs.unit.inlet.flow_mol_phase_comp[0, "Liq", "H2O"].fix(47.356)
+        # very confident
+        charge = {
+            "Li_+": 1,
+            "Mg_2+": 2,
+            "Cl_-": -1
+        },
+        # choose ideal for now, other option is davies
+        activity_coefficient_model=ActivityCoefficientModel.ideal,
+        density_calculation=DensityCalculation.constant,
+    )
 
-# fix the inlet fstate variables
-# kept all values from the WaterTAP test.py for now
-m.fs.unit.inlet.temperature[0].fix(298.15)
-m.fs.unit.inlet.pressure[0].fix(4e5)
+    # define unit models
+    # m.fs.pump = Pump(property_package=m.fs.properties)
+    m.fs.unit = NanofiltrationDSPMDE0D(property_package = m.fs.properties)
 
-# fix the membrane properties, typical for DSPM-DE
-# kept all values from the WaterTAP test.py for now
-m.fs.unit.radius_pore.fix(0.5e-9)
-m.fs.unit.membrane_thickness_effective.fix(1.33e-6)
-m.fs.unit.membrane_charge_density.fix(-27)
-m.fs.unit.dielectric_constant_pore.fix(41.3)
+    # fix the inlet flow rates
+    # kept all values from the WaterTAP test.py for now
+    m.fs.unit.feed_side.properties_in[0.0].flow_mol_phase_comp["Liq", "Li_+"].fix(0.429868)
+    m.fs.unit.feed_side.properties_in[0.0].flow_mol_phase_comp["Liq", "Mg_2+"].fix(0.429868)
+    m.fs.unit.feed_side.properties_in[0.0].flow_mol_phase_comp["Liq", "Cl_-"].fix(0.429868)
+    m.fs.unit.feed_side.properties_in[0.0].flow_mol_phase_comp["Liq", "H2O"].fix(47.356)
 
-# fix final permeate pressure to be approx atmospheric
-m.fs.unit.mixed_permeate[0].pressure.fix(101325)
+    # assert electroneutrality
+    m.fs.unit.feed_side.properties_in[0.0].assert_electroneutrality(
+        defined_state = True,
+        adjust_by_ion = "Cl_-",
+        get_property = "flow_mol_phase_comp"
+    )
 
-# fix system values
-# kept all values from the WaterTAP test.py for now
-m.fs.unit.spacer_porosity.fix(0.85)
-m.fs.unit.channel_height.fix(5e-4)
-m.fs.unit.velocity[0, 0].fix(0.25)
-m.fs.unit.area.fix(50)
+    # fix the inlet fstate variables
+    # kept all values from the WaterTAP test.py for now
+    m.fs.feed.properties[0].temperature.fix(298.15)
+    m.fs.feed.properties[0].pressure.fix(4e5)
 
-# fix additional variables for calculating mass transfer coefficient with spiral wound correlation
-m.fs.unit.spacer_mixing_efficiency.fix()
-m.fs.unit.spacer_mixing_length.fix()
+    # fix the membrane properties, typical for DSPM-DE
+    # kept all values from the WaterTAP test.py for now
+    m.fs.unit.radius_pore.fix(0.5e-9)
+    m.fs.unit.membrane_thickness_effective.fix(1.33e-6)
+    m.fs.unit.membrane_charge_density.fix(-27)
+    m.fs.unit.dielectric_constant_pore.fix(41.3)
 
-# assert electroneutrality
-m.fs.feed.properties[0].assert_electroneutrality(
-    defined_state=True,
-    adjust_by_ion="Cl_-",
-)
+    # fix final permeate pressure to be approx atmospheric
+    m.fs.unit.mixed_permeate[0].pressure.fix(101325)
 
-# check the DOF
-check_dof(m, fail_flag = True)
+    # fix system values
+    m.fs.unit.spacer_porosity.fix(0.85)
+    m.fs.unit.channel_height.fix(5e-4)
+    m.fs.unit.velocity[0, 0].fix(0.1)
+    m.fs.unit.area.fix(100)
 
-# scaling, using same method as WaterTAP test.py for now
-m.fs.properties.set_default_scaling(
-    "flow_mol_phase_comp", 1e2, index = ("Liq", "Li_+")
-)
-m.fs.properties.set_default_scaling(
-    "flow_mol_phase_comp", 1e2, index = ("Liq", "Mg_2+")
-)
-m.fs.properties.set_default_scaling(
-    "flow_mol_phase_comp", 1e2, index = ("Liq", "Cl_-")
-)
-m.fs.properties.set_default_scaling(
-    "flow_mol_phase_comp", 1e0, index = ("Liq", "H2O")
-)
-# calculate the scaling factors
-calculate_scaling_factors(m)
-# check that all variables have scaling factors
-unscaled_var_list = list(unscaled_variables_generator(m.fs.unit))
-assert len(unscaled_var_list) == 0
-# Expect only flux_mol_phase_comp to be poorly scaled, as we have not
-# calculated correct values just yet.
-for var in list(badly_scaled_var_generator(m.fs.unit)):
-    assert "flux_mol_phase_comp" in var[0].name
+    # fix additional variables for calculating mass transfer coefficient with spiral wound correlation
+    m.fs.unit.spacer_mixing_efficiency.fix()
+    m.fs.unit.spacer_mixing_length.fix()
 
-# from idaes.core.util.model_diagnostics import DiagnosticsToolbox
-# dt = DiagnosticsToolbox(m)
-# dt.report_structural_issues()
+    # check the DOF
+    check_dof(m, fail_flag = True)
 
-# initialize model
-initialization_tester(m)
+    # scaling, using same method as WaterTAP test.py for now
+    m.fs.properties.set_default_scaling(
+        "flow_mol_phase_comp", 1 / 0.5, index = ("Liq", "Li_+")
+    )
+    m.fs.properties.set_default_scaling(
+        "flow_mol_phase_comp", 1 / 0.5, index = ("Liq", "Mg_2+")
+    )
+    m.fs.properties.set_default_scaling(
+        "flow_mol_phase_comp", 1, index = ("Liq", "Cl_-")
+    )
+    m.fs.properties.set_default_scaling(
+        "flow_mol_phase_comp", 1 / 47, index = ("Liq", "H2O")
+    )
+    # calculate the scaling factors
+    calculate_scaling_factors(m)
+    # check that all variables have scaling factors
+    unscaled_var_list = list(unscaled_variables_generator(m.fs.unit))
+    assert len(unscaled_var_list) == 0
+    # Expect only flux_mol_phase_comp to be poorly scaled, as we have not
+    # calculated correct values just yet.
+    for var in list(badly_scaled_var_generator(m.fs.unit)):
+        assert "flux_mol_phase_comp" in var[0].name
+    return m
 
-# from idaes.core.util.model_diagnostics import DiagnosticsToolbox
-# dt = DiagnosticsToolbox(m)
-# dt.report_numerical_issues()
 
-# solve model
-results = solver.solve(m)
+if __name__ == "__main__":
+    main()
