0927667058876675a201246dd5b0bcd7d20911cb
Molly Dougher <mdoughe6@nd.edu>
2023-12-14 09:07:01 -0500

    adjust from Na to Li

--- watertap/examples/flowsheets/RO/RO_Li_Mg.py
+++ watertap/examples/flowsheets/RO/RO_Li_Mg.py
@@ -4,16 +4,16 @@
 from pyomo.environ import ConcreteModel
 from idaes.core import FlowsheetBlock
 from idaes.core.util.scaling import calculate_scaling_factors
-from watertap.property_models.NaCl_prop_pack import NaClParameterBlock
+from watertap.property_models.LiCl_prop_pack import LiClParameterBlock
 from watertap.unit_models.reverse_osmosis_0D import ReverseOsmosis0D
 from watertap.unit_models.reverse_osmosis_0D import ConcentrationPolarizationType
 from watertap.unit_models.reverse_osmosis_0D import MassTransferCoefficient
 
 
-# Create a concrete model, flowsheet, and NaCl property parameter block.
+# Create a concrete model, flowsheet, and LiCl property parameter block.
 m = ConcreteModel()
 m.fs = FlowsheetBlock(dynamic=False)
-m.fs.properties = NaClParameterBlock()
+m.fs.properties = LiClParameterBlock()
 
 # Add an RO unit to the flowsheet.
 m.fs.unit = ReverseOsmosis0D(
@@ -23,7 +23,7 @@ m.fs.unit = ReverseOsmosis0D(
     has_pressure_change=False)
 
 # Specify system variables.
-m.fs.unit.inlet.flow_mass_phase_comp[0, 'Liq', 'NaCl'].fix(0.035)  # mass flow rate of NaCl (kg/s)
+m.fs.unit.inlet.flow_mass_phase_comp[0, 'Liq', 'LiCl'].fix(0.035)  # mass flow rate of LiCl (kg/s)
 m.fs.unit.inlet.flow_mass_phase_comp[0, 'Liq', 'H2O'].fix(0.965)   # mass flow rate of water (kg/s)
 m.fs.unit.inlet.pressure[0].fix(50e5)                              # feed pressure (Pa)
 m.fs.unit.inlet.temperature[0].fix(298.15)                         # feed temperature (K)
@@ -34,7 +34,7 @@ m.fs.unit.permeate.pressure[0].fix(101325)                         # permeate pr
 
 # Set scaling factors for component mass flowrates.
 m.fs.properties.set_default_scaling('flow_mass_phase_comp', 1, index=('Liq', 'H2O'))
-m.fs.properties.set_default_scaling('flow_mass_phase_comp', 1e2, index=('Liq', 'NaCl'))
+m.fs.properties.set_default_scaling('flow_mass_phase_comp', 1e2, index=('Liq', 'LiCl'))
 
 # Calculate scaling factors.
 calculate_scaling_factors(m)
