a683903adfad606137b82d4bcc8d966ed17995e7
Molly Dougher <mdoughe6@nd.edu>
2023-12-21 11:49:01 -0500

    add values to the test dictionary

--- watertap/examples/flowsheets/nf_dspmde/tests/test_nf_brine.py
+++ watertap/examples/flowsheets/nf_dspmde/tests/test_nf_brine.py
@@ -14,11 +14,20 @@ def test_main():
     test_dict = {
         "pressure": [m.fs.pump.outlet.pressure[0] * 1e-5, 2.0],
         "area": [m.fs.unit.area, 985.283490],
-        "recovery": [
+        "nf_recovery": [
             m.fs.unit.recovery_vol_phase[0.0, "Liq"] * 100,
            9.983259,
         ],
-        "ion_ratio": [
+        "li_rejection": [
+            m.fs.unit.rejection_intrinsic_phase_comp[0,"Liq","Li_+"].value * 100,
+            3.694042,
+        ],
+        "feed_ion_ratio": [
+            (m.fs.feed.flow_mol_phase_comp[0,"Liq","Mg_2+"].value/0.024)
+            /(m.fs.feed.flow_mol_phase_comp[0,"Liq","Li_+"].value/0.0069),
+            0.507745
+        ],
+        "perm_ion_ratio": [
             (m.fs.permeate.flow_mol_phase_comp[0,"Liq","Mg_2+"].value/0.024)
             /(m.fs.permeate.flow_mol_phase_comp[0,"Liq","Li_+"].value/0.0069),
             0.493056
@@ -26,4 +35,3 @@ def test_main():
     }
     for model_result, testval in test_dict.values():
         assert pytest.approx(testval, rel=1e-3) == value(model_result)
-    print("done")
