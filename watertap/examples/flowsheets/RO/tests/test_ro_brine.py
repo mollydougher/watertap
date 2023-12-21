6c14c812d4dbbe962276e322c5afdf6d78fb53cd
Molly Dougher <mdoughe6@nd.edu>
2023-12-21 11:41:12 -0500

    add licl recovery to test results

--- watertap/examples/flowsheets/RO/tests/test_ro_brine.py
+++ watertap/examples/flowsheets/RO/tests/test_ro_brine.py
@@ -13,10 +13,14 @@ def test_main():
     m = main()
     test_dict = {
         "area": [m.fs.unit.area, 1126.7194556],
-        "recovery": [
+        "ro_recovery": [
             m.fs.unit.recovery_vol_phase[0.0, "Liq"] * 100,
            77.004208,
         ],
+        "licl_recovery": [
+            m.fs.unit.recovery_mass_phase_comp[0,"Liq","LiCl"].value*100,
+            7.519423,
+        ]
     }
     for model_result, testval in test_dict.values():
         assert pytest.approx(testval, rel=1e-3) == value(model_result)
